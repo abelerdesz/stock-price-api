b25089621ac73c0147afd2e96791a3990acf1708
Abel Erdesz <abel.erdesz@gmail.com>
2025-03-21 21:37:40 +0100

    use cron service in stock module

--- src/stock.controller.ts
+++ src/stock.controller.ts
@@ -1,29 +1,35 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Put, Param, NotFoundException } from '@nestjs/common';
 import { StockService } from './stock.service';
+import { CronService } from './cron.service';
 import { StockMovingAverageResponse } from './types/stock.types';
 import { StockSymbolDto } from './dto/stock.dto';
 
 @Controller('stock')
 export class StockController {
-  constructor(private readonly stockService: StockService) {}
+  constructor(
+    private readonly stockService: StockService,
+    private readonly cronService: CronService,
+  ) {}
 
   @Get(':symbol')
   async getSymbol(
     @Param()
     params: StockSymbolDto,
   ): Promise<StockMovingAverageResponse> {
+    return this.stockService.getStockWithPriceData(params.symbol);
+  }
+
+  @Put(':symbol')
+  async startStockTracking(
+    @Param()
+    params: StockSymbolDto,
+  ): Promise<{ message: string }> {
     const stock = await this.stockService.getOrCreateStock(params.symbol);
 
-    if (!stock) {
-      throw new NotFoundException(
-        `Stock with symbol ${params.symbol} not found`,
-      );
-    }
+    this.cronService.startStockPriceUpdates(params.symbol);
 
     return {
-      price: stock.price,
-      updatedAt: stock.updatedAt,
-      movingAverage: stock.movingAverage,
+      message: `Started tracking stock ${params.symbol}`,
     };
   }
 }
--- src/stock.module.ts
+++ src/stock.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { StockController } from './stock.controller';
 import { StockService } from './stock.service';
 import { PrismaService } from './prisma.service';
+import { CronService } from './cron.service';
+
 @Module({
   imports: [],
   controllers: [StockController],
-  providers: [StockService, PrismaService],
+  providers: [StockService, PrismaService, CronService],
 })
 export class StockModule {}
